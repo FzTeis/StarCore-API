98237b155b03c429cf6c31c81547d530816e7bdb
FzTeis <146758681+FzTeis@users.noreply.github.com>
2024-12-10 06:58:08 -0600

    Actualizar facebook.js

--- scrapers/facebook.js
+++ scrapers/facebook.js
@@ -1,28 +1,42 @@
-// scrapers/facebook.js
-const scrapers = require('@bochilteam/scraper');  // Usamos el scraper global
+const axios = require('axios');
+const cheerio = require('cheerio');
 
-// Función que descarga el video de Facebook
-async function downloadFacebookVideo(url) {
-    try {
-        const res = await scrapers.facebookdlv2(url);
-
-        if (res && Array.isArray(res.result)) {
-            // Intentar encontrar el video en 720p o 360p
-            const video = res.result.find(video => video.quality === '720p') ||
-                          res.result.find(video => video.quality === '360p');
-
-            if (video) {
-                return { status: true, url: video.url };
-            } else {
-                return { status: false, message: 'No se encontró ningún video con las calidades 720p ni 360p' };
-            }
-        } else {
-            return { status: false, message: 'Ocurrió un error al obtener el video' };
+const fb = {
+    dl: async (url) => {
+        const fetchUrl = `https://fsaver.net/download/?url=${url}`;
+        const headers = {
+            "Upgrade-Insecure-Requests": "1",
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36",
+            "sec-ch-ua": '"Chromium";v="130", "Google Chrome";v="130", "Not?A_Brand";v="99"',
+            "sec-ch-ua-mobile": "?0",
+            "sec-ch-ua-platform": '"Windows"'
+        };
+        try {
+            const response = await axios.get(fetchUrl, { headers });
+            const html = response.data;
+            const data = await fsaver.getData(html);
+            return data;
+        } catch (error) {
+            return { success: false, message: error.message };
         }
-    } catch (err) {
-        console.error(err);
-        return { status: false, message: 'Error al obtener el video desde Facebook' };
-    }
-}
+    },
+    getData: async (content) => {
+        try {
+            const baseUrl = 'https://fsaver.net';
+            const $ = cheerio.load(content);
+            const videoSrc = $('.video__item').attr('src');
 
-module.exports = { downloadFacebookVideo };
+            if (!videoSrc) throw new Error('Video tidak ditemukan.');
+let ress = { 
+status: true,
+creator: "I'm Fz",
+video: baseUrl + videoSrc 
+};
+            return ress; 
+        } catch (error) {
+            return { success: false, message: error.message };
+        }
+    }
+};
+
+module.exports = fb
