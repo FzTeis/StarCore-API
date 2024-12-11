204977d3e13634a5a5501285f7d34ec3f4e238cd
FzTeis <146758681+FzTeis@users.noreply.github.com>
2024-12-10 19:36:40 -0600

    Actualizar facebook.js

--- scrapers/facebook.js
+++ scrapers/facebook.js
@@ -1,42 +1,43 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
 
-const fb = {
-    dl: async (url) => {
-        const fetchUrl = `https://fsaver.net/download/?url=${url}`;
-        const headers = {
-            "Upgrade-Insecure-Requests": "1",
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36",
-            "sec-ch-ua": '"Chromium";v="130", "Google Chrome";v="130", "Not?A_Brand";v="99"',
-            "sec-ch-ua-mobile": "?0",
-            "sec-ch-ua-platform": '"Windows"'
-        };
-        try {
-            const response = await axios.get(fetchUrl, { headers });
-            const html = response.data;
-            const data = await fb.getData(html);
-            return data;
-        } catch (error) {
-            return { success: false, message: error.message };
-        }
-    },
-    getData: async (content) => {
-        try {
-            const baseUrl = 'https://fsaver.net';
-            const $ = cheerio.load(content);
-            const videoSrc = $('.video__item').attr('src');
-
-            if (!videoSrc) throw new Error('Video tidak ditemukan.');
-let ress = { 
-status: true,
-creator: "I'm Fz",
-video: baseUrl + videoSrc 
-};
-            return ress; 
-        } catch (error) {
-            return { success: false, message: error.message };
-        }
+async function fetchHtml(url) {
+    try {
+        const response = await axios.get(url);
+        return response.data;
+    } catch (error) {
+        throw new Error(`Error al obtener el HTML: ${error.message}`);
     }
-};
+}
 
-module.exports = fb
+function extractDownloadLinks(html) {
+    const $ = cheerio.load(html);
+    const downloadLinks = [];
+
+    $('a').each((index, element) => {
+        const href = $(element).attr('href');
+        if (href && href.includes('download')) {
+            downloadLinks.push(href);
+        }
+    });
+
+    return downloadLinks;
+}
+
+async function fbdl(url) {
+    try {
+        const html = await fetchHtml(url);
+        const downloadLinks = extractDownloadLinks(html);
+
+        if (downloadLinks.length === 0) {
+            throw new Error('No se encontraron enlaces de descarga.');
+        }
+
+        return { status: true,
+creator: "I'm Fz", data: downloadLinks }
+    } catch (error) {
+        throw new Error(`Error en fbdl: ${error.message}`);
+    }
+}
+
+module.exports = { fbdl };
